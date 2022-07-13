10b50f1838fbb48dbf569a19f553565b16a16125
LaughingLeader <laughingleader@gmail.com>
2022-07-13 18:52:10 -0500

    Add filter dropdowns for the controller version of the lobby as well

--- Mods/PartySizeEvolved_01888044-40fa-4250-8934-add840ac23b7/Story/RawFiles/Lua/Client/LobbyFixes.lua
+++ Mods/PartySizeEvolved_01888044-40fa-4250-8934-add840ac23b7/Story/RawFiles/Lua/Client/LobbyFixes.lua
@@ -1,18 +1,25 @@
 Ext.AddPathOverride("Public/Game/GUI/serverlist.swf", "Public/PartySizeEvolved_01888044-40fa-4250-8934-add840ac23b7/GUI/Overrides/serverlist.swf")
 Ext.AddPathOverride("Public/Game/GUI/serverlist_c.swf", "Public/PartySizeEvolved_01888044-40fa-4250-8934-add840ac23b7/GUI/Overrides/serverlist_c.swf")
 
-local addedDropdowns = false
+local addedDropdowns = {}
 
 --This adds more slot dropdowns (5-10), then selects 10, which updates the engine's player slot limit
-Ext.RegisterUITypeCall(26, "registerAnchorId", function (ui, event)
-	if not addedDropdowns then
+local function UpdateFilterDropdowns(ui, typeID)
+	if not addedDropdowns[typeID] then
 		local this = ui:GetRoot()
 		for i=3,8 do
 			this.addFilterDropDownOption(10, i, tostring(i + 2))
 		end
 		this.selectFilterDropDownEntry(10, 8)
-		addedDropdowns = true
+		addedDropdowns[typeID] = true
 	end
+end
+Ext.RegisterUITypeCall(26, "registerAnchorId", function (ui, event)
+	UpdateFilterDropdowns(ui, 26)
+end)
+
+Ext.RegisterUITypeCall(27, "registerAnchorId", function (ui, event)
+	UpdateFilterDropdowns(ui, 27)
 end)
 
 -- Ext.RegisterUITypeInvokeListener(26, "setSlotClient", function (ui, event, index, playerType, teamId, title, isReady, isMine)
