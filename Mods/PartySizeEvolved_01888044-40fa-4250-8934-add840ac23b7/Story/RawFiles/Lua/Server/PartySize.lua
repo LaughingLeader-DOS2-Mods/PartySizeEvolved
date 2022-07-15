584471a2bc0aa37fc35eaab31b706046f8ac4a07
LaughingLeader <laughingleader@gmail.com>
2022-07-15 16:04:32 -0500

    Update global settings var when the DB changes via the dialog menu
    Also update NumPlayers again, though it may not have an effect in-game

--- Mods/PartySizeEvolved_01888044-40fa-4250-8934-add840ac23b7/Story/RawFiles/Lua/Server/PartySize.lua
+++ Mods/PartySizeEvolved_01888044-40fa-4250-8934-add840ac23b7/Story/RawFiles/Lua/Server/PartySize.lua
@@ -1,18 +1,21 @@
-local _justModifiedDB = false
-
-Ext.RegisterOsirisListener("DB_Origins_MaxPartySize", 1, "after", function(size)
-	if size == 4 and not _justModifiedDB then
-		_justModifiedDB = true
-		TryLoadMultiplayerLimit()
-		Osi.DB_Origins_MaxPartySize:Delete(nil)
-		local settings = GetSettings()
-		if settings then
-			local partySize = settings.Global:GetVariable("PartySize", MAX_PLAYERS) or MAX_PLAYERS
-			Osi.DB_Origins_MaxPartySize(partySize)
-		else
-			Osi.DB_Origins_MaxPartySize(MAX_PLAYERS)
+--Update global settings when the DB changes from the dialog menu
+Ext.Osiris.RegisterListener("DialogEnded", 2, "after", function(dialog, id)
+	if dialog == "LLPARTY_SettingsMenu" then
+		local db = Osi.DB_LLPARTY_MaxPartySize:Get(nil)
+		if db and db[1] and type(db[1][1]) == "number" then
+			local amount = db[1][1]
+			if MAX_PLAYERS ~= amount then
+				MAX_PLAYERS = amount
+				Ext.Net.BroadcastMessage("LLPARTY_SetMaxPlayers", tostring(MAX_PLAYERS))
+				local settings = GetSettings()
+				if settings then
+					settings:SetVariable("PartySize", MAX_PLAYERS)
+					if Mods.LeaderLib.SaveGlobalSettings then
+						Mods.LeaderLib.SaveGlobalSettings()
+					end
+				end
+				SetNumPlayers(MAX_PLAYERS)
+			end
 		end
-		GlobalClearFlag("GEN_MaxPlayerCountReached")
 	end
-	_justModifiedDB = false
 end)
--- Mods/PartySizeEvolved_01888044-40fa-4250-8934-add840ac23b7/Story/RawFiles/Lua/Shared.lua
+++ Mods/PartySizeEvolved_01888044-40fa-4250-8934-add840ac23b7/Story/RawFiles/Lua/Shared.lua
@@ -2,9 +2,10 @@ local _ISCLIENT = Ext.IsClient()
 
 MAX_PLAYERS = 10
 
+---@return ModSettings
 function GetSettings()
 	if Mods.LeaderLib ~= nil then
-		local settings = Mods.LeaderLib.SettingsManager.GetMod(ModuleUUID, false)
+		local settings = Mods.LeaderLib.SettingsManager.GetMod(ModuleUUID, false, true)
 		if settings then
 			return settings
 		end
@@ -37,6 +38,13 @@ else
 	Ext.RegisterNetListener("LLPARTY_RepositionPortraits", function()
 		PlayerInfo.RepositionPortraits()
 	end)
+	Ext.RegisterNetListener("LLPARTY_SetMaxPlayers", function(cmd, payload)
+		local amount = tonumber(payload)
+		if amount then
+			MAX_PLAYERS = math.ceil(amount)
+			SetNumPlayers(MAX_PLAYERS)
+		end
+	end)
 end
 
 local GetCampaignInfo = function()
@@ -62,7 +70,7 @@ function TryLoadMultiplayerLimit()
 	end
 end
 
-local function SetNumPlayers(maxCount)
+function SetNumPlayers(maxCount)
 	if not maxCount then
 		TryLoadMultiplayerLimit()
 		maxCount = MAX_PLAYERS
@@ -88,3 +96,55 @@ end)
 Ext.Events.ResetCompleted:Subscribe(function (e)
 	SetNumPlayers()
 end)
+
+---@param amount integer
+local function OnPartySizeChanged(amount)
+	if amount > 0 then
+		MAX_PLAYERS = amount
+		if not _ISCLIENT then
+			Osi.DB_LLPARTY_MaxPartySize:Delete(nil)
+			Osi.DB_Origins_MaxPartySize:Delete(nil)
+			Osi.DB_Origins_MaxPartySize(amount)
+			Osi.DB_LLPARTY_MaxPartySize(amount)
+			Osi.Proc_CheckPartyFull()
+			Osi.LLPARTY_InstallMod()
+			Osi.LLPARTY_Settings_UpdateDialogVars()
+			GlobalClearFlag("GEN_MaxPlayerCountReached")
+		end
+	end
+end
+
+---@param partySizeVar VariableData
+local function RegisterVariableChangedListener(partySizeVar)
+	if partySizeVar then
+		OnPartySizeChanged(partySizeVar.Value)
+		if partySizeVar.Subscribe then
+			partySizeVar:Subscribe(function (e)
+				OnPartySizeChanged(e.Value)
+			end)
+		elseif partySizeVar.AddListener then
+			partySizeVar:AddListener(function (id, value, data, settings)
+				OnPartySizeChanged(value)
+			end)
+		end
+	end
+end
+
+Ext.Events.SessionLoaded:Subscribe(function (e)
+	if Mods.LeaderLib then
+		if Mods.LeaderLib.Events and Mods.LeaderLib.Events.ModSettingsLoaded then
+			local index = nil
+			index = Mods.LeaderLib.Events.ModSettingsLoaded:Subscribe(function (e)
+				RegisterVariableChangedListener(e.Settings.Global.Variables.PartySize)
+				Mods.LeaderLib.Events.ModSettingsLoaded:Unsubscribe(index)
+			end, {MatchArgs={UUID = ModuleUUID}})
+		else
+			Ext.OnNextTick(function (e)
+				local settings = GetSettings()
+				if settings then
+					RegisterVariableChangedListener(settings.Global.Variables.PartySize)
+				end
+			end)
+		end
+	end
+end, {Priority=1000})
